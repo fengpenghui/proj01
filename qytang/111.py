dd2c425c830ace0483d8cb7c8a241172ccaff68d
fengph <jcyhdream@163.com>
2019-07-07 03:01:40 +0800

    1111111

--- qytang/111.py
+++
@@ -1,5 +0,0 @@
-#! /usr/bin/env python3
-# coding: utf-8
-# github: https://github.com/fengpenghui
-# 码云: https://gitee.com/fengpenghui0923
-
--- qytang/qytang_multicmd.py
+++ qytang/qytang_multicmd.py
@@ -7,30 +7,23 @@
 import paramiko
 import re
 
-def qytang_paramiko_ssh(ip, username, password, port=22, cmd='ls'):
 def qytang_multicmd(ip, username, password, cmd_list, enable='', wait_time=2, verbose=True):
     ssh = paramiko.SSHClient()
     ssh.load_system_host_keys()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    ssh.connect(ip, port=port, username=username, password=password, timeout=5, compress=True)
-    stdin, stdout, stderr = ssh.exec_command(cmd)
-    return  stdout.read().decode()
-
-def ssh_get_route(ip,username,password):
-    route_result = qytang_ssh(ip,username,password,cmd='route -n')
-    for route in route_result.split('\n')[2:-1]:
-        re_route = re.match(r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\s+'
-                            r'(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\s+'
-                            r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\s+'
-                            r'(\w+)\s+\d+\s+\d+\s+\d+ \w+',
-                            route.strip())
-        if re_route:
-            if re_route.groups()[1] == 'UG':
-                return re_route.groups()[0]
+    ssh.connect('192.168.2.11', port=22, username='cisco', password='cisco', timeout=5, compress=True)
+    chan = ssh.invoke_shell()
+    time.sleep(1)
+    chan.recv(2048).decode()
+    chan.send('config ter'.encode())  # 发送命令
+    chan.send(b'\n')  # 回车
+    x = chan.recv(2048).decode()  # 获取路由器返回的信息
+    print(x)  # 打印返回信息
+    chan.send('router ospf 1'.encode())
+    chan.send(b'\n')
+    x = chan.recv(2048).decode()
+    print(x)
 
 if __name__ == '__main__':
-    print(qytang_ssh('10.1.1.12','root', 'P@ssw0rd'))
- #   print(qytang_ssh('192.168.2.11','cisco', 'cisco'))
-    print(qytang_ssh('10.1.1.12','root', 'P@ssw0rd',cmd='pwd'))
-    print('网关为：')
-    print(ssh_get_route('10.1.1.12', 'root', 'P@ssw0rd'))
+    cmd_list = ('show version','hostname Test')
+    print(qytang_multicmd('192.168.2.11', username='cisco', password='cisco'))
